d87ac3ae83b02d671fe2fd1e905b750decd72af9
knightromantic <jiepiguo@gmail.com>
2024-11-14 19:58:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,12 +13,12 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '他们假装努力和进步，而我开始少说多做',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '永垂不朽，然后死去',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'JP', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '18岁，男，大一，INFP，性情闷骚，喜特立独行；爱技术，爱艺术，爱生活', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '闲散青年，爱技术，爱艺术', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://jpblog.link', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客，技术分享，日常', // 网站关键词 英文逗号隔开
 
