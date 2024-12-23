07823787f11d087782c4f43c9c12c0d34ea4cc56
knightromantic <jiepiguo@gmail.com>
2024-12-23 15:47:38 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,7 +13,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '🎉🎉🎉欢迎来到我的世界！我是Jason，一个对音乐、文学和电影充满热情的18岁大学生，现就读于上海，专业是工业工程。我相信真理存在于人文与科技的交界处，在这里我将分享我的学习心得、生活感悟和对未来的思考。希望我的博客能为你带来启发，期待我们共同成长！',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '欢迎来到我的世界！我是Jason，一个对音乐、文学和电影充满热情的18岁大学生，现就读于上海，专业是工业工程。我相信真理存在于人文与科技的交界处，在这里我将分享我的学习心得、生活感悟和对未来的思考。希望我的博客能为你带来启发，期待我们共同成长！',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
