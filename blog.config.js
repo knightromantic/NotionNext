338468dfbb4a5f6399e9b355b6cd1b76df10be2e
knightromantic <jiepiguo@gmail.com>
2024-12-23 21:11:42 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,12 +13,12 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '欢迎来到我的世界！我是Jason，一个对音乐、文学和电影充满热情的18岁大学生，现就读于上海，专业是工业工程。我相信真理存在于人文与科技的交界处，在这里我将分享我的学习心得、生活感悟和对未来的思考。希望我的博客能为你带来启发，期待我们共同成长！',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '求知若渴 虚心若愚',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Jason.P', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Stay hungry，Stay foolish', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '欢迎来到我的世界！我是Jason，一个对音乐、文学和电影充满热情的18岁大学生，现就读于上海，专业是工业工程。我相信真理存在于人文与科技的交界处，在这里我将分享我的学习心得、生活感悟和对未来的思考。希望我的博客能为你带来启发，期待我们共同成长！', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://jpblog.link', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客，技术分享，日常，个人IP', // 网站关键词 英文逗号隔开
 
